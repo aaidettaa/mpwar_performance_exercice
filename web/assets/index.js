ff39a40e7295115e2ffb299eefe3f9f1e64f72a3
Xavier Domingo Burjachs <xavierdomingoburjachs@gmail.com>
2016-05-21 19:04:22 +0200

    Unused bootstrap.js file removed.

--- web/assets/index.js
+++ web/assets/index.js
@@ -2,4 +2,3 @@ require('./components/bootstrap/dist/css/bootstrap.css');
 require('./css/blog.css');
 
 require('./components/jquery/dist/jquery.js');
-require('./components/bootstrap/dist/js/bootstrap.js');
