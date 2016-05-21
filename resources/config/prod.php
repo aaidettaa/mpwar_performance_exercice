f8d88ecce6c75fd1b60904edc1b3394e52adcd56
aaidettaa <aida.aso@gmail.com>
2016-05-21 21:53:51 +0200

    update config file

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -7,12 +7,30 @@ $app['twig.options'] = [];
 
 $app['db.options'] = [
     "driver"    => "pdo_mysql",
-    "host"      => 'mysql',
-    "user"      => 'root',
-    "password"  => 'root',
+    "host"      => 'localhost',
+    "user"      => 'your-user',
+    "password"  => 'your-password',
     "dbname"    => 'mpwar_performance_blog',
     "charset"   => "utf8"
 ];
+
+$app['redis.options'] = [
+    'host'     => 'localhost',
+    'port'     => 6379,
+    'timeout'  => 0,
+    'dbname'   => 'mpwar_performance_blog',
+    'password' => ''
+];
+
+$app['imageAwsS3.options'] = [
+    'key'     => 'your-key',
+    'secret'     => 'your-secret',
+    'region'  => 'eu-west-1',
+    'version'   => 'latest',
+    'bucketName' => 'your-bucket-name',
+    'prefix' => 'optional-prefix'
+];
+
 $app['orm.proxies_dir'] = '/tmp/proxies';
 $app['orm.auto_generate_proxies'] = true;
 $app['orm.em.options'] = [
