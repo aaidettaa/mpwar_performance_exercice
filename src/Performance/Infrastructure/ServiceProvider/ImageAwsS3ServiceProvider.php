48e887c7218e641266b5428b4b0f9b6331618d4d
aaidettaa <aida.aso@gmail.com>
2016-05-21 21:45:26 +0200

    create service provider

---
+++ src/Performance/Infrastructure/ServiceProvider/ImageAwsS3ServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Performance\Infrastructure\ServiceProvider;
+
+
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Config;
+use League\Flysystem\Filesystem;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ImageAwsS3ServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $pimple)
+    {
+        $pimple['imageAwsS3'] = function () use ($pimple) {
+
+            $options = $pimple['imageAwsS3.options'];
+
+            $client = new S3Client([
+                'credentials' => [
+                    'key'    => $options['key'],
+                    'secret' => $options['secret'],
+                ],
+                'region' => $options['region'],
+                'version' => $options['version'],
+            ]);
+
+            $aws3adapter = new AwsS3Adapter($client, $options['bucketName'], $options['prefix']);
+
+            $filesystem = new Filesystem($aws3adapter, new Config([]));
+
+            return $filesystem;
+        };
+    }
+}
